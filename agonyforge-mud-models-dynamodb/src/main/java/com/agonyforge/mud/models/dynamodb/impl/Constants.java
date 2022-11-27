4f9b93ab8d3ca1c55fe093ac59aececf8f4a7ad1
Peter Keeler <peter@bonevm.com>
2022-11-26 21:17:54 -0800

    zones don't do much yet

--- agonyforge-mud-models-dynamodb/src/main/java/com/agonyforge/mud/models/dynamodb/impl/Constants.java
+++ agonyforge-mud-models-dynamodb/src/main/java/com/agonyforge/mud/models/dynamodb/impl/Constants.java
@@ -13,6 +13,7 @@ public class Constants {
 
     public static final String DB_USER = "USER#";
     public static final String DB_PC = "PC#";
+    public static final String DB_ZONE = "ZONE#";
 
     public static final String SORT_DATA = "DATA";
     public static final String SORT_SESSION = "SESSION#";
@@ -23,4 +24,5 @@ public class Constants {
     public static final String TYPE_SESSION = "SESSION";
     public static final String TYPE_PC = "PC";
     public static final String TYPE_COMMAND = "COMMAND";
+    public static final String TYPE_ZONE = "ZONE";
 }
