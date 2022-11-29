53153c8f970db406ae53ca57629ef928c7dc897f
Peter Keeler <peter@bonevm.com>
2022-11-29 11:19:49 -0800

    try using testcontainers to avoid embedded DynamoDbLocal
    will it work in CodeBuild? 🤷‍

--- agonyforge-mud-models-dynamodb/src/test/java/com/agonyforge/mud/models/dynamodb/repository/DynamoDbLocalInitializingTest.java
+++ agonyforge-mud-models-dynamodb/src/test/java/com/agonyforge/mud/models/dynamodb/repository/DynamoDbLocalInitializingTest.java
@@ -2,20 +2,18 @@ package com.agonyforge.mud.models.dynamodb.repository;
 
 import com.agonyforge.mud.models.dynamodb.DynamoDbInitializer;
 import com.agonyforge.mud.models.dynamodb.config.DynamoDbProperties;
-import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
-import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-import java.net.ServerSocket;
 import java.net.URI;
 
 import static org.mockito.Mockito.lenient;
@@ -23,38 +21,33 @@ import static org.mockito.Mockito.lenient;
 @ExtendWith(MockitoExtension.class)
 public abstract class DynamoDbLocalInitializingTest {
     protected static DynamoDbClient dynamoDbClient;
-    protected static DynamoDBProxyServer server;
+    protected static final GenericContainer dynamoDbLocal;
+
+    static {
+        DockerImageName imageName = DockerImageName.parse("amazon/dynamodb-local:latest");
+        dynamoDbLocal = new GenericContainer(imageName)
+            .withExposedPorts(8000);
+        dynamoDbLocal.start();
+    }
 
     @Mock
     protected DynamoDbProperties.TableNames tableNames;
 
     @BeforeAll
     static void setUpAll() throws Exception {
-        String port;
-
-        try (ServerSocket serverSocket = new ServerSocket(0)) {
-            port = String.valueOf(serverSocket.getLocalPort());
-        }
-
         dynamoDbClient = DynamoDbClient
             .builder()
-            .endpointOverride(new URI("http://localhost:" + port))
+            .endpointOverride(new URI(String.format("http://%s:%s",
+                dynamoDbLocal.getHost(),
+                dynamoDbLocal.getFirstMappedPort())))
             .region(Region.US_WEST_2)
             .credentialsProvider(
                 StaticCredentialsProvider.create(
                     AwsBasicCredentials.create("x", "x")))
             .build();
 
-        server = ServerRunner.createServerFromCommandLineArgs(new String[] { "-inMemory", "-port", port });
-        server.start();
-
         // create tables and stuff
         new DynamoDbInitializer(dynamoDbClient).initialize();
-    }
-
-    @AfterAll
-    static void tearDownAll() throws Exception {
-        server.stop();
     }
 
     @BeforeEach
