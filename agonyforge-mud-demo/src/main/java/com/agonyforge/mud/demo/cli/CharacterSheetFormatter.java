b3270b8b361b2e7e02774c6186edfc815bde4b2e
Peter Keeler <peter@bonevm.com>
2023-06-05 23:22:49 -0700

    nice formatted character sheet with colors and stuff

--- agonyforge-mud-demo/src/main/java/com/agonyforge/mud/demo/cli/CharacterSheetFormatter.java
+++ agonyforge-mud-demo/src/main/java/com/agonyforge/mud/demo/cli/CharacterSheetFormatter.java
@@ -1,6 +1,7 @@
 package com.agonyforge.mud.demo.cli;
 
 import com.agonyforge.mud.core.web.model.Output;
+import com.agonyforge.mud.models.dynamodb.constant.Effort;
 import com.agonyforge.mud.models.dynamodb.constant.Stat;
 import com.agonyforge.mud.models.dynamodb.impl.MudCharacter;
 
@@ -12,13 +13,23 @@ public final class CharacterSheetFormatter {
     }
 
     public static void format(MudCharacter ch, Output output) {
-        output.append("[dcyan]Character Sheet");
+        output.append("[dcyan]CHARACTER SHEET");
         output.append("[default]Name: [cyan]%s", ch.getName());
         output.append("[default]Pronouns: [cyan]%s/%s", ch.getPronoun().getSubject(), ch.getPronoun().getObject());
+        output.append("");
+        output.append("[cyan]Stats   [magenta]Efforts");
+        for (int i = 0; i < Math.max(Stat.values().length, Effort.values().length); i++) {
+            Stat stat = Stat.values().length > i ? Stat.values()[i] : null;
+            Effort effort = Effort.values().length > i ? Effort.values()[i] : null;
 
-        Arrays.stream(Stat.values())
-            .forEachOrdered(stat -> output.append("[default]%s: [cyan]%d", stat.getAbbreviation(), ch.getStat(stat)));
+            String statString = stat != null ? String.format("[default]%s: [cyan]%d", stat.getAbbreviation(), ch.getStat(stat)) : "";
+            String effortString = effort != null ? String.format("[default](d%-2d) %-15s: [magenta]%d", effort.getDie(), effort.getName(), ch.getEffort(effort)) : "";
 
-        output.append("[default]DEF: [cyan]%d", ch.getDefense());
+            output.append("%15s\t%15s", statString, effortString);
+        }
+
+        output.append("");
+        output.append("[default]Health: [red]❤");
+        output.append("[default]DEF: [green]%d", ch.getDefense());
     }
 }
