2e9ac64ac6bc1beb74830cf0628fcf21e9b89499
Peter Keeler <scion@agonyforge.com>
2024-12-12 18:12:39 -0800

    remove unused import

--- agonyforge-mud-core/src/main/java/com/agonyforge/mud/core/config/StompClientConfiguration.java
+++ agonyforge-mud-core/src/main/java/com/agonyforge/mud/core/config/StompClientConfiguration.java
@@ -1,6 +1,5 @@
 package com.agonyforge.mud.core.config;
 
-import com.agonyforge.mud.core.config.MqBrokerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
