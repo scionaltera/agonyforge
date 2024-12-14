8c04a7a189e8076ad724467db6625e69346cadb7
Peter Keeler <scion@agonyforge.com>
2024-12-13 17:02:08 -0800

    remove dynamodb

--- agonyforge-mud-demo/src/main/java/com/agonyforge/mud/demo/model/repository/CommandRepository.java
+++ agonyforge-mud-demo/src/main/java/com/agonyforge/mud/demo/model/repository/CommandRepository.java
@@ -1,28 +1,8 @@
 package com.agonyforge.mud.demo.model.repository;
 
-import com.agonyforge.mud.models.dynamodb.config.DynamoDbProperties;
 import com.agonyforge.mud.demo.model.impl.CommandReference;
-import com.agonyforge.mud.models.dynamodb.repository.AbstractRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.model.ComparisonOperator;
-import software.amazon.awssdk.services.dynamodb.model.Condition;
-import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
-import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
-import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.agonyforge.mud.demo.model.impl.ModelConstants.TYPE_COMMAND;
 
 @Repository
 public interface CommandRepository extends JpaRepository<CommandReference, String> {
