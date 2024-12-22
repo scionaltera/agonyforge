1720b85731d6b5f446e6ef46dfb0ebb665533322
Peter Keeler <scion@agonyforge.com>
2024-12-22 02:24:38 -0800

    pipe access logs into the regular mud logs

---
+++ agonyforge-mud-core/src/main/java/com/agonyforge/mud/core/config/TomcatConfiguration.java
@@ -0,0 +1,18 @@
+package com.agonyforge.mud.core.config;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        LogbackValve valve = new LogbackValve();
+        valve.setQuiet(false);
+
+        factory.addEngineValves(valve);
+        factory.addContextValves(valve);
+    }
+}
