5dd469bccd0c39dbd5c3efe232cce4a4c0a17920
Peter Keeler <scion@agonyforge.com>
2024-12-26 22:35:41 -0800

    remove unnecessary import

--- agonyforge-mud-core/src/main/java/com/agonyforge/mud/core/config/RemoteIpHandshakeInterceptor.java
+++ agonyforge-mud-core/src/main/java/com/agonyforge/mud/core/config/RemoteIpHandshakeInterceptor.java
@@ -2,7 +2,6 @@ package com.agonyforge.mud.core.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
