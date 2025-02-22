2267692a6af94a3301ef79bf7a17c706fff59956
Peter Keeler <scion@agonyforge.com>
2025-02-21 23:43:10 -0800

    better impl of AuditAwareImpl

--- agonyforge-mud-demo/src/main/java/com/agonyforge/mud/demo/config/AuditorAwareImpl.java
+++ agonyforge-mud-demo/src/main/java/com/agonyforge/mud/demo/config/AuditorAwareImpl.java
@@ -1,27 +1,21 @@
 package com.agonyforge.mud.demo.config;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null) {
-            return Optional.empty();
-        }
-
-        Object principal = authentication.getPrincipal();
-
-        if (principal == null) {
-            return Optional.empty();
-        }
-
-        return Optional.of(((AuthenticatedPrincipal) principal).getName());
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(Authentication::isAuthenticated)
+            .map(Authentication::getPrincipal)
+            .map(OAuth2User.class::cast)
+            .map(OAuth2User::getName);
     }
 }
