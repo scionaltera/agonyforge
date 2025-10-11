eda2f164340dcae11d001fe3a73ebcbd79da76db
Peter Keeler <scion@agonyforge.com>
2025-10-10 18:32:11 -0700

    test: add skeleton tests for MudItemTemplate

---
+++ agonyforge-mud-demo/src/test/java/com/agonyforge/mud/demo/model/impl/MudItemTemplateTest.java
@@ -0,0 +1,28 @@
+package com.agonyforge.mud.demo.model.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MudItemTemplateTest {
+    @Test
+    void testBuildInstance() {
+        // AI!
+    }
+
+    @Test
+    void testId() {
+
+    }
+
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void testHashCode() {
+
+    }
+}
